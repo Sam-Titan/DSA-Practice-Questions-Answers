d2cbda9523f2b791c3737e062246fc9d517d99b5
Sam-Titan <bansalswayam2005@gmail.com>
2026-04-26 15:47:28 +0530

    Duplicate Check Question

---
+++ Amazon_Prep/Duplicate_Check.py
@@ -0,0 +1,22 @@
+# Pattern D: Existence Check (Has this been seen before?)
+# Problem type: Find first duplicate, detect if a value exists, check intersection.
+
+def check(nums):
+    seen = set()
+    for num in nums:
+        if num in seen:
+            return True
+        seen.add(num)
+    return False
+
+print(check([1,2,3,4,5,1]))
+
+def check(nums):
+    seen = {}
+    for num in nums:
+        if num in seen:
+            return True
+        seen[num] = seen.get(num, 0) + 1
+    return False
+
+print(check([1,2,3,4,5,1]))
